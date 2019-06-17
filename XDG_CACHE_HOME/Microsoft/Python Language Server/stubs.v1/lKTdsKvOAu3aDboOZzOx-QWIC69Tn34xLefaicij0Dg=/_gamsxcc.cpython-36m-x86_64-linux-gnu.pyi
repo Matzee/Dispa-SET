d7a99526503cb3ac85b7f32693d58fa603047ff5
Matthias Zech <matthias.zech@gmail.com>
2019-06-17 08:02:37 +0200

    further ideas

---
+++ XDG_CACHE_HOME/Microsoft/Python Language Server/stubs.v1/lKTdsKvOAu3aDboOZzOx-QWIC69Tn34xLefaicij0Dg=/_gamsxcc.cpython-36m-x86_64-linux-gnu.pyi
@@ -0,0 +1,387 @@
+GLOBAL_MAX_INDEX_DIM = 20
+GLOBAL_UEL_IDENT_SIZE = 64
+GMS_DT_ALIAS = 4
+GMS_DT_EQU = 3
+GMS_DT_MAX = 5
+GMS_DT_PAR = 1
+GMS_DT_SET = 0
+GMS_DT_VAR = 2
+GMS_EQUTYPE_C = 5
+GMS_EQUTYPE_E = 0
+GMS_EQUTYPE_G = 1
+GMS_EQUTYPE_L = 2
+GMS_EQUTYPE_MAX = 6
+GMS_EQUTYPE_N = 3
+GMS_EQUTYPE_X = 4
+GMS_MAX_INDEX_DIM = 20
+GMS_SSSIZE = 256
+GMS_SVIDX_ACR = 6
+GMS_SVIDX_EPS = 4
+GMS_SVIDX_MAX = 7
+GMS_SVIDX_MINF = 3
+GMS_SVIDX_NA = 1
+GMS_SVIDX_NORMAL = 5
+GMS_SVIDX_PINF = 2
+GMS_SVIDX_UNDEF = 0
+GMS_SV_ACR = 1e+301
+GMS_SV_EPS = 5e+300
+GMS_SV_MINF = 4e+300
+GMS_SV_NA = 2e+300
+GMS_SV_NAINT = 2100000000
+GMS_SV_PINF = 3e+300
+GMS_SV_UNDEF = 1e+300
+GMS_UEL_IDENT_SIZE = 64
+GMS_VAL_LEVEL = 0
+GMS_VAL_LOWER = 2
+GMS_VAL_MARGINAL = 1
+GMS_VAL_MAX = 5
+GMS_VAL_SCALE = 4
+GMS_VAL_UPPER = 3
+GMS_VARTYPE_BINARY = 1
+GMS_VARTYPE_FREE = 5
+GMS_VARTYPE_INTEGER = 2
+GMS_VARTYPE_MAX = 10
+GMS_VARTYPE_NEGATIVE = 4
+GMS_VARTYPE_POSITIVE = 3
+GMS_VARTYPE_SEMICONT = 8
+GMS_VARTYPE_SEMIINT = 9
+GMS_VARTYPE_SOS1 = 6
+GMS_VARTYPE_SOS2 = 7
+GMS_VARTYPE_UNKNOWN = 0
+ITERLIM_INFINITY = 2000000000
+def SWIG_PyInstanceMethod_New():
+    pass
+
+def TBrkPCallBack1_tp_assign():
+    pass
+
+def TBrkPCallBack1_tp_value():
+    pass
+
+def TBrkPCallBack2_tp_assign():
+    pass
+
+def TBrkPCallBack2_tp_value():
+    pass
+
+__doc__ = None
+__file__ = '/home/matthias/anaconda3/envs/dispaset/lib/python3.6/site-packages/_gamsxcc.cpython-36m-x86_64-linux-gnu.so'
+__name__ = '_gamsxcc'
+__package__ = ''
+def copy_TBrkPCallBack1_tp():
+    pass
+
+def copy_TBrkPCallBack2_tp():
+    pass
+
+def copy_doublep():
+    pass
+
+def copy_gamsxHandle_tp():
+    pass
+
+def copy_intp():
+    pass
+
+def delete_TBrkPCallBack1_tp():
+    pass
+
+def delete_TBrkPCallBack2_tp():
+    pass
+
+def delete_doubleArray():
+    pass
+
+def delete_doublep():
+    pass
+
+def delete_gamsxHandle_tp():
+    pass
+
+def delete_intArray():
+    pass
+
+def delete_intp():
+    pass
+
+def doubleArray___getitem__():
+    pass
+
+def doubleArray___setitem__():
+    pass
+
+def doubleArray_cast():
+    pass
+
+def doubleArray_frompointer():
+    pass
+
+def doubleArray_swigregister():
+    pass
+
+def doublep_assign():
+    pass
+
+def doublep_value():
+    pass
+
+dt_alias = 4
+dt_equ = 3
+dt_par = 1
+dt_set = 0
+dt_var = 2
+def gamsxAddBreakPoint(pgamsx, fn, lineNr):
+    'gamsxAddBreakPoint(pgamsx, fn, lineNr)'
+    pass
+
+def gamsxCB1Defined(pgamsx):
+    'gamsxCB1Defined(pgamsx) -> int'
+    return 1
+
+def gamsxCB2Defined(pgamsx):
+    'gamsxCB2Defined(pgamsx) -> int'
+    return 1
+
+def gamsxClearBreakPoints(pgamsx):
+    'gamsxClearBreakPoints(pgamsx)'
+    pass
+
+def gamsxCreate(pgamsx, msgBufSize):
+    'gamsxCreate(pgamsx, msgBufSize) -> int'
+    return 1
+
+def gamsxCreateD(pgamsx, dirName, msgBufSize):
+    'gamsxCreateD(pgamsx, dirName, msgBufSize) -> int'
+    return 1
+
+def gamsxCreateL(pgamsx, libName, msgBufSize):
+    'gamsxCreateL(pgamsx, libName, msgBufSize) -> int'
+    return 1
+
+def gamsxDataReadDone(pgamsx):
+    'gamsxDataReadDone(pgamsx) -> int'
+    return 1
+
+def gamsxDataReadRaw(pgamsx, INOUT):
+    'gamsxDataReadRaw(pgamsx, INOUT) -> int'
+    return 1
+
+def gamsxDataReadRawStart(pgamsx, SyNr, INOUT):
+    'gamsxDataReadRawStart(pgamsx, SyNr, INOUT) -> int'
+    return 1
+
+def gamsxDataWriteDone(pgamsx):
+    'gamsxDataWriteDone(pgamsx) -> int'
+    return 1
+
+def gamsxDataWriteRaw(pgamsx, Elements, Vals):
+    'gamsxDataWriteRaw(pgamsx, Elements, Vals) -> int'
+    return 1
+
+def gamsxDataWriteRawStart(pgamsx, SyNr, DoMerge):
+    'gamsxDataWriteRawStart(pgamsx, SyNr, DoMerge) -> int'
+    return 1
+
+def gamsxErrorHandling(msg):
+    'gamsxErrorHandling(msg)'
+    pass
+
+def gamsxFindSymbol(pgamsx, SyName):
+    'gamsxFindSymbol(pgamsx, SyName) -> int'
+    return 1
+
+def gamsxFree(pgamsx):
+    'gamsxFree(pgamsx) -> int'
+    return 1
+
+def gamsxGetAPIErrorCount():
+    'gamsxGetAPIErrorCount() -> int'
+    return 1
+
+def gamsxGetCB1(pgamsx):
+    'gamsxGetCB1(pgamsx) -> TBrkPCallBack1_t'
+    pass
+
+def gamsxGetCB1UM(pgamsx):
+    'gamsxGetCB1UM(pgamsx) -> void *'
+    pass
+
+def gamsxGetCB2(pgamsx):
+    'gamsxGetCB2(pgamsx) -> TBrkPCallBack2_t'
+    pass
+
+def gamsxGetCB2UM1(pgamsx):
+    'gamsxGetCB2UM1(pgamsx) -> void *'
+    pass
+
+def gamsxGetCB2UM2(pgamsx):
+    'gamsxGetCB2UM2(pgamsx) -> void *'
+    pass
+
+def gamsxGetErrorCallback():
+    'gamsxGetErrorCallback() -> gamsxErrorCallback_t'
+    pass
+
+def gamsxGetExceptionIndicator():
+    'gamsxGetExceptionIndicator() -> int'
+    return 1
+
+def gamsxGetExitIndicator():
+    'gamsxGetExitIndicator() -> int'
+    return 1
+
+def gamsxGetReady(msgBufSize):
+    'gamsxGetReady(msgBufSize) -> int'
+    return 1
+
+def gamsxGetReadyD(dirName, msgBufSize):
+    'gamsxGetReadyD(dirName, msgBufSize) -> int'
+    return 1
+
+def gamsxGetReadyL(libName, msgBufSize):
+    'gamsxGetReadyL(libName, msgBufSize) -> int'
+    return 1
+
+def gamsxGetScreenIndicator():
+    'gamsxGetScreenIndicator() -> int'
+    return 1
+
+def gamsxHandleToPtr(pgamsx):
+    'gamsxHandleToPtr(pgamsx) -> void *'
+    pass
+
+def gamsxHandle_tp_assign():
+    pass
+
+def gamsxHandle_tp_value():
+    pass
+
+def gamsxLibraryLoaded():
+    'gamsxLibraryLoaded() -> int'
+    return 1
+
+def gamsxLibraryUnload():
+    'gamsxLibraryUnload() -> int'
+    return 1
+
+def gamsxRegisterCB1(pgamsx, CB1, userMem):
+    'gamsxRegisterCB1(pgamsx, CB1, userMem)'
+    pass
+
+def gamsxRegisterCB2(pgamsx, CB2, userMem1, userMem2):
+    'gamsxRegisterCB2(pgamsx, CB2, userMem1, userMem2)'
+    pass
+
+def gamsxRunExecDLL(pgamsx, optPtr, sysDir, AVerbose):
+    'gamsxRunExecDLL(pgamsx, optPtr, sysDir, AVerbose) -> int'
+    return 1
+
+def gamsxRunToEnd(pgamsx):
+    'gamsxRunToEnd(pgamsx) -> int'
+    return 1
+
+def gamsxRunToEndSet(pgamsx, x):
+    'gamsxRunToEndSet(pgamsx, x)'
+    pass
+
+def gamsxSWSet(pgamsx, x):
+    'gamsxSWSet(pgamsx, x)'
+    pass
+
+def gamsxSetAPIErrorCount(ecnt):
+    'gamsxSetAPIErrorCount(ecnt)'
+    pass
+
+def gamsxSetErrorCallback(func):
+    'gamsxSetErrorCallback(func)'
+    pass
+
+def gamsxSetExceptionIndicator(excind):
+    'gamsxSetExceptionIndicator(excind)'
+    pass
+
+def gamsxSetExitIndicator(extind):
+    'gamsxSetExitIndicator(extind)'
+    pass
+
+def gamsxSetScreenIndicator(scrind):
+    'gamsxSetScreenIndicator(scrind)'
+    pass
+
+def gamsxShowError(pgamsx, fNameLog):
+    'gamsxShowError(pgamsx, fNameLog) -> int'
+    return 1
+
+def gamsxStepThrough(pgamsx):
+    'gamsxStepThrough(pgamsx) -> int'
+    return 1
+
+def gamsxStepThroughSet(pgamsx, x):
+    'gamsxStepThroughSet(pgamsx, x)'
+    pass
+
+def gamsxSymbolInfo(pgamsx, SyNr, arg5, arg6, arg7, arg8):
+    'gamsxSymbolInfo(pgamsx, SyNr, arg5, arg6, arg7, arg8) -> int'
+    return 1
+
+def gamsxSystemInfo(pgamsx, arg2, arg3):
+    'gamsxSystemInfo(pgamsx, arg2, arg3) -> int'
+    return 1
+
+def gamsxUelName(pgamsx, uel):
+    'gamsxUelName(pgamsx, uel) -> char *'
+    pass
+
+def intArray___getitem__():
+    pass
+
+def intArray___setitem__():
+    pass
+
+def intArray_cast():
+    pass
+
+def intArray_frompointer():
+    pass
+
+def intArray_swigregister():
+    pass
+
+def intp_assign():
+    pass
+
+def intp_value():
+    pass
+
+def new_TBrkPCallBack1_tp():
+    pass
+
+def new_TBrkPCallBack2_tp():
+    pass
+
+def new_doubleArray():
+    pass
+
+def new_doublep():
+    pass
+
+def new_gamsxHandle_tp():
+    pass
+
+def new_intArray():
+    pass
+
+def new_intp():
+    pass
+
+def ptrTogamsxHandle(vptr):
+    'ptrTogamsxHandle(vptr) -> gamsxHandle_t'
+    pass
+
+sv_acronym = 6
+sv_normal = 5
+sv_valeps = 4
+sv_valmin = 3
+sv_valna = 1
+sv_valpin = 2
+sv_valund = 0
